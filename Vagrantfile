dcdf9336714ee3bf96d6347de889c330a34a43b8
helpermethod <wo6980@gmail.com>
2014-10-09 07:52:15 +0100

    Added commandline argument support.

--- Vagrantfile
+++ Vagrantfile
@@ -2,5 +2,5 @@ VAGRANTFILE_API_VERSION = "2"
 
 Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   config.vm.box = "ubuntu/trusty64"
-  config.vm.provision :shell, privileged: false, path: "bootstrap.sh"
+  config.vm.provision :shell, privileged: false, path: "bootstrap.sh", args: ENV[SHELL_ARGS]
 end
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,14 +1,34 @@
 #!/usr/bin/env bash
 
+# parse args 
+while :; do
+	case $1 in
+		--harmony)
+		--vim)
+		-*)
+	esac
+done
+
 sudo apt-get update
 sudo apt-get install -y git
 
 curl --silent https://raw.githubusercontent.com/creationix/nvm/v0.17.0/install.sh | sh
 . ~/.nvm/nvm.sh
-nvm install 0.11.14
-nvm alias default 0.11.14
+
+node_version
+
+if [[ $harmony ]]; then
+	node_version='0.11.14'
+	# runs node with all harmony flags enabled
+	printf "alias node='node --harmony'" >> ~/.bashrc
+else
+	node_version='0.10.32'
+fi
+
+nvm install "$node_version"
+nvm alias default "$node_version"
+
 # enables tab completion for nvm
 printf '[[ -r "$NVM_DIR"/bash_completion ]] && . "$NVM_DIR"/bash_completion\n' >> ~/.bashrc
 # enables tab completion for npm
 printf '. <(npm completion)\n' >> ~/.bashrc
-printf "alias node='node --harmony'" >> ~/.bashrc
